224ecc21418340a722d73044b44537a4c9f0a0af
Arun-mozhi <cj.arunmozhi@gmail.com>
2020-11-01 20:01:29 +0530

    practice files

---
+++ home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+
+
+<?php
+$x = 5;
+$y = 5;
+//echo "combine x and y  = ";
+//echo $x + $y;
+
+$t = 10;
+
+if ($t < $x) {
+  //echo "yes";
+} else {
+  //echo "no";
+}
+
+for ($i = 1; $i <= 2; $i++) {
+  echo "test: $i <br>";
+}
+
+?>
+</body>
+</html>
